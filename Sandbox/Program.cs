375e40d2e23d58223a45d1ffd711579727d1e4c4
Ivan Malakhov <vanhall360@gmail.com>
2021-04-13 17:15:17 +0700

    Process stream redirect test

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Sandbox
 {
@@ -6,7 +8,32 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ydlProcess = new Process();
+            ydlProcess.StartInfo.FileName = "youtube-dl.exe";
+            ydlProcess.StartInfo.UseShellExecute = false;
+            ydlProcess.StartInfo.RedirectStandardOutput = true;
+
+            ydlProcess.OutputDataReceived += YdlProcess_OutputDataReceived;
+
+            ydlProcess.StartInfo.RedirectStandardInput = true;
+
+            ydlProcess.Start();
+
+            StreamWriter inputStream = ydlProcess.StandardInput;
+
+            ydlProcess.BeginOutputReadLine();
+
+            Console.WriteLine("Input to pass to yt-dl:");
+            var userInput = Console.ReadLine();
+            inputStream.Write(userInput);
+            inputStream.Close();
+
+            ydlProcess.WaitForExit();
+        }
+
+        private static void YdlProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
         }
     }
 }
