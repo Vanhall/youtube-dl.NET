9046058b1257da425e7b892441dc6e1048aa6d9b
Ivan Malakhov <vanhall360@gmail.com>
2021-04-13 18:14:10 +0700

    Process stream redirect test 2

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,38 +1,45 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
+using System.Threading;
 
 namespace Sandbox
 {
     class Program
     {
+        static bool ydlExited = false;
         static void Main(string[] args)
         {
             var ydlProcess = new Process();
             ydlProcess.StartInfo.FileName = "youtube-dl.exe";
             ydlProcess.StartInfo.UseShellExecute = false;
             ydlProcess.StartInfo.RedirectStandardOutput = true;
-
+            ydlProcess.EnableRaisingEvents = true;
             ydlProcess.OutputDataReceived += YdlProcess_OutputDataReceived;
-
-            ydlProcess.StartInfo.RedirectStandardInput = true;
-
-            ydlProcess.Start();
-
-            StreamWriter inputStream = ydlProcess.StandardInput;
-
-            ydlProcess.BeginOutputReadLine();
+            ydlProcess.Exited += YdlProcess_Exited;
 
             Console.WriteLine("Input to pass to yt-dl:");
             var userInput = Console.ReadLine();
-            inputStream.Write(userInput);
-            inputStream.Close();
+            ydlProcess.StartInfo.Arguments = userInput;
 
-            ydlProcess.WaitForExit();
+            ydlProcess.Start();
+            ydlProcess.BeginOutputReadLine();
+
+            while (!ydlExited)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Host Process writing");
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void YdlProcess_Exited(object sender, EventArgs e)
+        {
+            ydlExited = true;
         }
 
         private static void YdlProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
+            Console.ResetColor();
             Console.WriteLine(e.Data);
         }
     }
