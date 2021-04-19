f42ba6f99fd82fcf3c3179837ad387db240fa21d
Ivan Malakhov <vanhall360@gmail.com>
2021-04-19 21:40:24 +0700

    forgot new file :)

---
+++ Youtube-dl.Net/Parsers/TestParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeDl
+{
+    public class TestParser : IYdlOutputParser<string>
+    {
+        public string GetResult()
+        {
+            return "[Parser] Done";
+        }
+
+        public void Parse(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+                Console.WriteLine("[Task] " + input);
+        }
+    }
+}
