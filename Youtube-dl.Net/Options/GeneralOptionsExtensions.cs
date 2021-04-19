6a3e757b04b433d8fca2da8959c1e449cb747359
Ivan Malakhov <vanhall360@gmail.com>
2021-04-19 17:24:42 +0700

    Renamed project folder

---
+++ Youtube-dl.Net/Options/GeneralOptionsExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace YoutubeDl
+{
+    public static class GeneralOptionsExtensions
+    {
+        public static Options IgnoreConfig(this Options o) => o.AddOption(new SwitchOption("--ignore-config"));
+
+        public static Options Version(this Options o) => o.AddOption(new SwitchOption("--version"));
+    }
+}
