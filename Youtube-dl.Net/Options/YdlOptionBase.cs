a9a9937fa24eb4208c13eca39216270f373af0ee
Ivan Malakhov <vanhall360@gmail.com>
2021-04-21 15:23:07 +0700

    Namespaces fix

---
+++ Youtube-dl.Net/Options/YdlOptionBase.cs
@@ -0,0 +1,16 @@
+﻿namespace YoutubeDl
+{
+    public abstract class OptionBase : IYdlOption
+    {
+        protected string key;
+
+        public override int GetHashCode() => key.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is OptionBase other)
+                return key.Equals(other.key);
+            return false;
+        }
+    }
+}
---
+++ Youtube-dl.Net/Options/YdlOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace YoutubeDl
+{
+    public class Options
+    {
+        private readonly HashSet<IYdlOption> options = new HashSet<IYdlOption>();
+
+        public bool TryAddOption(IYdlOption option) => options.Add(option);
+
+        public Options AddOption(IYdlOption option)
+        {
+            options.Add(option);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", options);
+        }
+    }
+}
